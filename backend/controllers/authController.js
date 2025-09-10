67c4cf83928c6896a850632a46534e91a21dec78
Seron Senapati <seronsenapati@gmail.com>
2025-09-11 02:33:22 +0530

    Fix company association check in frontend and backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import Company from "../models/Company.js"; // Add this import
 import dotenv from "dotenv";
 import sendResponse from "../utils/sendResponse.js";
 
